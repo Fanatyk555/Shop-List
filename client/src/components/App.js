a3beaae0009c215f0ac948de705fea32363333de
Fanatyk555 <62016328+Fanatyk555@users.noreply.github.com>
2021-08-08 23:55:40 +0200

    new components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,55 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { PieChart } from 'react-minimal-pie-chart';
 import React from 'react';
 import axios from 'axios';
-
+const Search = (props) => {
+  return(
+    <div className="selectProduct">
+    <label>Wyszukaj produkt:</label>
+    <input onChange={props.handleInput} value={props.value}/>
+    <ul className="inputDropList">{props.searchList}</ul>
+  </div>
+  )
+}
+const ProductList = (props) => {
+  return(
+    <div style={{overflowX: "auto"}}>
+      <table className="table table-sm table-striped">
+      {/* table-responsive */}
+        <thead>
+        <tr>
+          <th scope="col" style={{backgroundColor: "#2eb8b8"}}>Nazwa</th>
+          <th scope="col" style={{backgroundColor: "#2eb8b8"}}>Cena</th>
+          <th scope="col" style={{backgroundColor: "#99cc00"}}>Waga netto</th>
+          <th scope="col" style={{backgroundColor: "#99cc00"}}>Waga 1szt</th>
+          <th scope="col" style={{backgroundColor: "#99cc00"}}>Waga</th>
+          <th scope="col" style={{backgroundColor: "#e6e600"}}>Kalorie</th>
+          <th scope="col" style={{backgroundColor: "#E38627"}}>Tłuszcze</th>
+          <th scope="col" style={{backgroundColor: "#C13C37"}}>Węglowodany</th>
+          <th scope="col" style={{backgroundColor: "#6A2135"}}>Białko</th>
+          <th scope="col" style={{backgroundColor: "#333333"}}></th>
+        </tr>
+        </thead>
+        <tbody>
+          {props.productsList}
+          <tr style={{backgroundColor: "#808080"}}>
+            <td>Podsumowanie:</td>
+            <td>{Math.round(props.price*1e2) / 1e2} zł</td>
+            <td>{props.weight} g</td>    
+            <td></td>    
+            <td></td>    
+            <td>{Math.round(props.calories*1e1) / 1e1} kcal</td>    
+            <td>{Math.round(props.fat*1e1) / 1e1} g</td>    
+            <td>{Math.round(props.carbohydrates*1e1) / 1e1} g</td>
+            <td>{Math.round(props.protein*1e1) / 1e1} g</td>    
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
 class App extends React.Component {
   state = {
     apiResponse: [],
@@ -31,17 +76,17 @@ class App extends React.Component {
   render() {
     const productsList = this.state.productsList.map((item, index)=>(
       <tr key={index}>
-        <td>{item.value}</td>
-        <td>{Math.round((((item.price/item.weight)*this.state.productsWeight[index]))* 1e2) / 1e2}zł</td>
-        <td>{item.weight}g</td>  
-        <td>{item.weight_1szt}g</td>  
-        <td className="selectWeight"><input type="number" value={this.state.productsWeight[index]} onChange={(e)=>{this.setState(({productsWeight}) => ({
-          productsWeight: [...productsWeight.slice(0,index),[e.target.value],...productsWeight.slice(index+1)]}))}}/></td>  
-        <td>{Math.round((((item.calories/100)*this.state.productsWeight[index]))* 1e1) / 1e1}</td>    
-        <td>{Math.round((((item.fat/100)*this.state.productsWeight[index]))* 1e1) / 1e1}</td>    
-        <td>{Math.round((((item.carbohydrates/100)*this.state.productsWeight[index]))* 1e1) / 1e1}</td>
-        <td>{Math.round((((item.protein/100)*this.state.productsWeight[index]))* 1e1) / 1e1}</td> 
-        <td><button onClick={(e)=>{this.setState(({productsWeight, productsList}) => ({
+        <td style={{backgroundColor: "#2eb8b8"}}>{item.value}</td>
+        <td style={{backgroundColor: "#2eb8b8"}}>{Math.round((((item.price/item.weight)*this.state.productsWeight[index]))* 1e2) / 1e2} zł</td>
+        <td style={{backgroundColor: "#99cc00"}}>{item.weight} g</td>  
+        <td style={{backgroundColor: "#99cc00"}}>{item.weight_1szt} g</td>  
+        <td className="selectWeight" style={{backgroundColor: "#99cc00"}}><input type="number" value={this.state.productsWeight[index]} onChange={(e)=>{this.setState(({productsWeight}) => ({
+          productsWeight: [...productsWeight.slice(0,index),[e.target.value],...productsWeight.slice(index+1)]}))}}/> g</td>  
+        <td style={{backgroundColor: "#e6e600"}}>{Math.round((((item.calories/100)*this.state.productsWeight[index]))* 1e1) / 1e1} kcal</td>    
+        <td style={{backgroundColor: "#E38627"}}>{Math.round((((item.fat/100)*this.state.productsWeight[index]))* 1e1) / 1e1} g</td>    
+        <td style={{backgroundColor: "#C13C37"}}>{Math.round((((item.carbohydrates/100)*this.state.productsWeight[index]))* 1e1) / 1e1} g</td>
+        <td style={{backgroundColor: "#6A2135"}}>{Math.round((((item.protein/100)*this.state.productsWeight[index]))* 1e1) / 1e1} g</td> 
+        <td style={{backgroundColor: "#333333"}}><button onClick={(e)=>{this.setState(({productsWeight, productsList}) => ({
           productsWeight: [...productsWeight.slice(0,index),...productsWeight.slice(index+1)],
           productsList: [...productsList.slice(0,index),...productsList.slice(index+1)]}))}}>x</button></td>   
       </tr>));
@@ -67,47 +112,19 @@ class App extends React.Component {
       </>);
     return (
       <>
-        <div className="container-fluid text-white">
-          <div className="selectProduct">
-            <label>Wyszukaj produkt:</label>
-            <input onChange={this.handleInputProduct} value={this.state.value}/>
-            <ul className="inputDropList">{searchProduct}</ul>
-          </div>
+        <div className="container-fluid text-dark">
+          <Search handleInput={this.handleInputProduct} value={this.state.value} searchList={searchProduct}/>
           <br/>
-          {this.state.productsList.length!==0?
-          <>
-            <table className="table table-sm">
-              <thead>
-              <tr>
-                <th scope="col">Nazwa</th>
-                <th scope="col">Cena</th>
-                <th scope="col">Waga netto</th>
-                <th scope="col">Waga 1szt</th>
-                <th scope="col">Waga</th>
-                <th scope="col">Kalorie</th>
-                <th scope="col">Tłuszcze</th>
-                <th scope="col">Węglowodany</th>
-                <th scope="col">Białko</th>
-                <th scope="col"></th>
-              </tr>
-              </thead>
-              <tbody>
-                {productsList}
-                <label>Podsumowanie:</label>
-                <tr>
-                  <td></td>
-                  <td>{Math.round(price*1e2) / 1e2}zł</td>
-                  <td>{weight}g</td>    
-                  <td></td>    
-                  <td></td>    
-                  <td>{Math.round(calories*1e1) / 1e1}</td>    
-                  <td>{Math.round(fat*1e1) / 1e1}</td>    
-                  <td>{Math.round(carbohydrates*1e1) / 1e1}</td>
-                  <td>{Math.round(protein*1e1) / 1e1}</td>    
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+          {this.state.productsList.length!==0?<>
+            <ProductList 
+              stateProductsList={this.state.productsList} 
+              stateProductsWeight={this.state.productsWeight}
+              productsList={productsList}
+              calories={calories}
+              fat={fat}
+              carbohydrates={carbohydrates}
+              protein={protein}
+            />
             <PieChart
               data={[
                 { title: 'Tłuszcze', value: Math.round(fat*1e1) / 1e1, color: '#E38627' },
@@ -118,10 +135,7 @@ class App extends React.Component {
               radius={20}
               center={[50,20]}
               label={({ dataEntry }) => dataEntry.title+": "+dataEntry.value+"g"}
-              labelStyle={{
-                fontSize: "2.5px"
-              }}
-              
+              labelStyle={{ fontSize: "2.5px" }}
             />
           </>:null}
         </div>
